b93f577d758ceced63154fa647ea99b05901f175
swap1930 <swapnildudhane1930@gmail.com>
2025-07-09 22:20:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1108,4 +1108,5 @@ def firebase_login():
 
 if __name__ == '__main__':
     setup_database()
-    app.run(debug=True) 
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host='0.0.0.0', port=port)
